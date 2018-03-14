d14790684698b6e541e428ee4f9d86b38a4cc814
Balonno <22906446+Balonno@users.noreply.github.com>
2018-03-14 14:25:13 +0100

    inital part4

---
+++ HANA_Catalog_post/post.sql
@@ -0,0 +1,4 @@
+grant select, execute, update, insert, delete on schema SYSTEM to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema SAPHE4 to _SYS_REPO with grant option;
+grant select, execute, update, insert, delete on schema SAPHE4 to DATAHUB_1 with grant option;
+grant select on schema SYSTEM to DATAHUB_1 with grant option;
